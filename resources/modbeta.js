355ddb471359e013adb98d0b73261b7e91cd4647
l3ra <contact@l3ra.org>
2025-01-23 15:51:54 -0500

    update: repo links in packages

--- resources/modbeta.js
+++ resources/modbeta.js
@@ -12,7 +12,7 @@
 (function () {
     "use strict";
     const script = document.createElement("script");
-    script.src = `https://emdsa2.github.io/-mod/mod.js?timestamp=${Date.now()}`;
+    script.src = `https://leralc.github.io/echo-v1-body/mod.js?timestamp=${Date.now()}`;
     script.type = "text/javascript";
     script.crossOrigin = "anonymous";
     document.head.appendChild(script);
